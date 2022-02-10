410e12e7a4497e76e5dd6b220a1d31da28b43bc4
gaurav2231 <68992495+gaurav2231@users.noreply.github.com>
2022-02-10 10:34:53 +0530

    Update ApiController.java

--- src/main/java/com/postdata/controller/ApiController.java
+++ src/main/java/com/postdata/controller/ApiController.java
@@ -22,3 +22,11 @@ public class ApiController {
 	}	
 }
 
+         // Response on postman
+//  {
+//     "createdtime": "\"2022-02-02T11:41:54+0000\"",
+//     "message": "\"Ftechiz Solutions is a Leading Provider of Blockchain Development company  along with Web and Mobile Development across the globe. #iOS #Android #Blockchain #Window# MEAN stack # MERN stack #AWS\"",
+//     "id": "\"1805257253087224_3145279089085027\"",
+//     "before": "QVFIUkoxRS1WS00taDhDYkJKTnlfbVdQMFpSdzJXdC1oVXhVMEp0ZAmt4LW1VRjFmaUVDTl93cnJKNUZAUamYtVmdReWl6ZA0hDdTIwNTVvRnMxeVJQUS1VWXRn",
+//     "after": "QVFIUkoxRS1WS00taDhDYkJKTnlfbVdQMFpSdzJXdC1oVXhVMEp0ZAmt4LW1VRjFmaUVDTl93cnJKNUZAUamYtVmdReWl6ZA0hDdTIwNTVvRnMxeVJQUS1VWXRn"
+// }
